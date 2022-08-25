058c5ec4c40749fcc0fd5da3e8d26e5ddb7276c1
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2022-08-25 10:57:15 +0200

    Added Testing Repo

---
+++ scripts/ttf-ms-win10/ttf-ms-win10.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+touch license.rtf
+wget -O fonts.zip "https://github.com/3omar-mostafa/Windows-ISO-Extractor/releases/download/21H2_x64/Windows10_21H2_English.International_x64_Fonts.zip"
+unzip -uj fonts.zip
