4866f90d1f7d7178cef4586db3215c5212d8be9a
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2022-09-11 00:11:48 +0200

    Disabled pacman XferCommand

--- scripts/pacmanconf.sh
+++ scripts/pacmanconf.sh
@@ -14,6 +14,6 @@ sed -i 's|#Include = /etc/pacman.d/mirrorlist|Include = /etc/pacman.d/mirrorlist
 
 echo '[options]' >> /etc/pacman.conf
 echo 'ParallelDownloads = 5' >> /etc/pacman.conf
-echo 'XferCommand = /usr/bin/aria2c --allow-overwrite=true --continue=true --file-allocation=none --log-level=error --max-tries=3 --max-connection-per-server=2 --max-file-not-found=5 --min-split-size=5M --no-conf --remote-time=true --summary-interval=60 --timeout=50 --dir=/ --out %o %u' >> /etc/pacman.conf
+# echo 'XferCommand = /usr/bin/aria2c --allow-overwrite=true --continue=true --file-allocation=none --log-level=error --max-tries=3 --max-connection-per-server=2 --max-file-not-found=5 --min-split-size=5M --no-conf --remote-time=true --summary-interval=60 --timeout=50 --dir=/ --out %o %u' >> /etc/pacman.conf
 
 bat --color always --pager=never /etc/pacman.conf
