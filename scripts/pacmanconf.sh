8c09b71c3eedad0a1bedc6dd4d3756128bf7d251
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2022-09-10 23:45:47 +0200

    Fixed Enabeling multilib repos

--- scripts/pacmanconf.sh
+++ scripts/pacmanconf.sh
@@ -8,8 +8,8 @@ sed -i 's/IgnorePkg/#IgnorePkg/g' /etc/pacman.conf
 sed -i 's/#VerbosePkgLists/VerbosePkgLists/g' /etc/pacman.conf
 
 # Enable multilib repos
-sed -i 's/#[multilib]/[multilib]/g' /etc/pacman.conf
-sed -i 's|#Include = /etc/pacman.d/mirrorlist|#Include = /etc/pacman.d/mirrorlist|g' /etc/pacman.conf
+sed -i 's/#\[multilib\]/\[multilib\]/g' /etc/pacman.conf
+sed -i 's|#Include = /etc/pacman.d/mirrorlist|Include = /etc/pacman.d/mirrorlist|g' /etc/pacman.conf
 
 echo '[options]' >> /etc/pacman.conf
 echo 'ParallelDownloads = 5' >> /etc/pacman.conf
