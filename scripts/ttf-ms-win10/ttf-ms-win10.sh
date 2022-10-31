807ebb2c78ce90f8b0e095adf0c8990c6b607662
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2022-10-31 22:34:18 +0100

    Updated Windows Fonts

--- scripts/ttf-ms-win10/ttf-ms-win10.sh
+++ scripts/ttf-ms-win10/ttf-ms-win10.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 touch license.rtf
-wget -O fonts.zip "https://github.com/3omar-mostafa/Windows-ISO-Extractor/releases/download/21H2_x64/Windows10_21H2_English.International_x64_Fonts.zip"
+wget -O fonts.zip "https://github.com/3omar-mostafa/Windows-ISO-Extractor/releases/download/22H2_x64/Windows10_22H2_English.International_x64_Fonts.zip"
 unzip -uj fonts.zip
