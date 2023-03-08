ce6bea4e85296ccad2934a8a0d7493c254463e85
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2023-03-08 22:13:53 +0100

    Updated Packages

--- scripts/postman/postman.sh
+++ scripts/postman/postman.sh
@@ -3,5 +3,6 @@
 SCRIPT_DIR=$(dirname -- "$0")
 
 patch -Np1 --ignore-whitespace -i "$SCRIPT_DIR/PKGBUILD.patch"
-sed -i "s/sha256sums=(.*/sha256sums=('SKIP'/g" PKGBUILD
+sed -i "s/sha256sums_x86_64=(.*/sha256sums_x86_64=('SKIP'/g" PKGBUILD
+sed -i "s/sha256sums_aarch64=(.*/sha256sums_aarch64=('SKIP'/g" PKGBUILD
 
