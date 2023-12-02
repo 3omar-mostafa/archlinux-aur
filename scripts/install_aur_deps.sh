e35a6fa9f3b099495c650b92bdc85f5156dc91f7
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2023-12-02 13:28:11 +0200

    Upgrade paru to v2 & Remove checkdepends

--- scripts/install_aur_deps.sh
+++ scripts/install_aur_deps.sh
@@ -3,8 +3,5 @@
 cd /tmp && git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin  && makepkg -si --noconfirm --clean
 
-cd /tmp && git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin  && makepkg -si --noconfirm --clean
-
 cd /tmp && git clone https://aur.archlinux.org/downgrade.git
 cd downgrade  && makepkg -si --noconfirm --clean
