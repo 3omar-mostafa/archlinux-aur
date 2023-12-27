230fbe87541f06523a44b54ff7203f31479ec66c
3omar-mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2023-12-27 23:02:46 +0200

    Allow changing paru/yay per application

--- scripts/install_aur_deps.sh
+++ scripts/install_aur_deps.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+cd /tmp && git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin  && makepkg -si --noconfirm --clean
+
 cd /tmp && git clone https://aur.archlinux.org/paru-bin.git
 cd paru-bin  && makepkg -si --noconfirm --clean
 
