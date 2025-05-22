00d85aca8fa61f7b5005669920f4f325b34acc0b
Omar Mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2025-05-22 14:53:04 +0300

    Update build.sh

--- scripts/apindex/build.sh
+++ scripts/apindex/build.sh
@@ -14,5 +14,5 @@ patch -Np1 --ignore-whitespace -i "$SCRIPT_DIR/apindex.patch"
 # Use arch icon for packages
 cp "$SCRIPT_DIR/package.png" ./share/apindex/img/package.png
 
-cmake . -DCMAKE_INSTALL_PREFIX=/usr
+cmake . -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 make install
